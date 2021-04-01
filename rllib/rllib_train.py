1d7d3bcb7cbcfa19ca9e4103e3cec3c5d317a309
Bryan Yu <bryayu@tik42x.ee.ethz.ch>
2021-04-01 15:24:07 +0200

    Adding metrics to callback

--- rllib/rllib_train.py
+++ rllib/rllib_train.py
@@ -1,29 +1,37 @@
 import ray
 from ray import tune
 from ray.tune import grid_search
-from ray.rllib.env.multi_agent_env import MultiAgentEnv
 from ray.rllib.utils.test_utils import check_learning_achieved
+from ray.rllib.models import ModelCatalog
+
 
 import argparse
 
-from ray.rllib.models import ModelCatalog
 from custom_model import Custom_Model
+from custom_callback import MyCallbacks
 from env import Volunteers_Dilemma
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--run", type=str, default="PG")
-parser.add_argument("--as-test", action="store_true")
+parser.add_argument("--as-test",    action="store_true")
 parser.add_argument("--local-mode", action="store_true")
-parser.add_argument("--n-agents", type=int, default=1)
-parser.add_argument("--n-workers", type=int, default=1)
-parser.add_argument("--n-gpus", type=int, default=0)
+parser.add_argument("--debug",      action="store_true")
+parser.add_argument("--run",        type=str, default="PG")
+parser.add_argument("--n-agents",   type=int, default=1)
+parser.add_argument("--n-workers",  type=int, default=1)
+parser.add_argument("--n-gpus",     type=int, default=0)
 parser.add_argument("--stop-iters", type=int, default=300)
 parser.add_argument("--stop-reward", type=float, default=6)
+parser.add_argument("--episode-length", type=int, default=1)
+parser.add_argument("--haircut-multiplier", type=int, default=0.50)
+
 
 if __name__ == "__main__":
     args = parser.parse_args()
-    args.log_dir = f"./results/{args.n_agents}_agents/{args.run}"
+    if args.debug:
+        args.log_dir = f"./results/DEBUG/{args.n_agents}_agents/{args.run}/episode_length_{args.episode_length}"
+    else:
+        args.log_dir = f"./results/{args.n_agents}_agents/{args.run}/episode_length_{args.episode_length}"
     ray.init(local_mode = args.local_mode)
 
     env_config = {
@@ -32,8 +40,8 @@ if __name__ == "__main__":
                                 [0.0, 0.0, 0.0],
                                 [15.0, 15.0, 0.0]],
             "position" :        [20.0, 20.0, 29.0],
-            "haircut_multiplier" : 0.50,
-            "episode_length" : 1,
+            "haircut_multiplier" : args.haircut_multiplier,
+            "episode_length" : args.episode_length,
         }
 
     env = Volunteers_Dilemma(env_config)
@@ -41,7 +49,6 @@ if __name__ == "__main__":
     action_space = env.action_space
 
     ModelCatalog.register_custom_model("my_torch_model", Custom_Model)
-
 
     config = {
         "env": Volunteers_Dilemma,  
@@ -56,10 +63,11 @@ if __name__ == "__main__":
         "model":{"custom_model": "my_torch_model",
                 "custom_model_config": {},
         },
-        "num_workers": args.n_workers,  # parallelism
+        "num_workers": args.n_workers,  
         "framework": "torch",
         "num_gpus": args.n_gpus,
-        "lr": tune.grid_search([1e-5,5e-5,1e-6,5e-6,1e-7]),
+        "lr": tune.grid_search([1e-5,5e-6]),
+        "callbacks": MyCallbacks,
         
     }
 
@@ -73,7 +81,6 @@ if __name__ == "__main__":
                         stop=stop, 
                         local_dir=args.log_dir, 
                         log_to_file="results.log",
-
                     )
 
     if args.as_test:
