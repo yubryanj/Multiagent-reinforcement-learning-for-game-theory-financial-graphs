376f980276c93231e532897adde7dc0a499d2f4a
Bryan Yu <bryany@kth.se>
2022-03-23 11:00:28 -0700

    cleaning up directory structure

--- rllib/scripts/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-source /itet-stor/bryayu/net_scratch/conda/etc/profile.d/conda.sh
-conda activate pku
-
-python trainer.py \
-    --experiment-number $1
--- rllib/scripts/run_pooled.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-source /itet-stor/bryayu/net_scratch/conda/etc/profile.d/conda.sh
-conda activate pku
-
-python trainer_pooled.py \
-    --local-mode \
-    --experiment-number $1
